991f1e73f6f8d8efee2de42daa7c13ea5b8620a3
Roman Zipp <ich@ich.wtf>
2022-09-21 11:44:28 +0200

    Add check app password

--- controllers/web/auth.go
+++ controllers/web/auth.go
@@ -3,6 +3,7 @@ package web
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/romanzipp/wanderer/application"
+	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
@@ -10,10 +11,18 @@ func ShowAuthFormController(c *gin.Context, app *application.App) {
 	c.HTML(http.StatusOK, "auth", gin.H{
 		"title": "Authenticate",
 		"nav":   "index",
+		"error": c.Query("error"),
 	})
 }
 
 func SubmitAuthController(c *gin.Context, app *application.App) {
+	password := c.PostForm("password")
+	if password != app.Env.Password {
+		log.Info().Msg("failed auth attempt")
+		c.Redirect(302, "/auth?error=Invalid+password")
+		return
+	}
+
 	c.SetCookie("token", "ok", 3000, "/", "", false, true)
 	c.Redirect(302, "/")
 }
